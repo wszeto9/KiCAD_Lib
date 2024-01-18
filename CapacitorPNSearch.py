5944504a18a77b1c167c1d487b2d8f88661fd16a
Winnie Szeto <87886980+wszeto9@users.noreply.github.com>
2024-01-18 17:51:18 -0500

    added edge cases

--- CapacitorPNSearch.py
+++ CapacitorPNSearch.py
@@ -1,13 +1,23 @@
-
-
-Capacitors = [["100nF", "50V", "0603", ""], ["220nF", "50V", "0603", ""], ["1uF", "50V", "0603", "1276-1860-1-ND"], ["2.2uF", "16V", "0603", "1276-1040-1-ND"], ["22uF", "10V", "0603", "1276-1274-1-ND"], ["10uF", "25V", "0603", "1276-1869-1-ND"]]
-
+Capacitors = [
+    ["100nF", "50V", "0603", "We have this at home"], 
+    ["220nF", "50V", "0603", "We have this at home"], 
+    ["1uF", "50V", "0603", "1276-1860-1-ND"], 
+    ["2.2uF", "16V", "0603", "1276-1040-1-ND"], 
+    ["22uF", "10V", "0603", "1276-1274-1-ND"], 
+    ["10uF", "25V", "0603", "1276-1869-1-ND"], 
+    ["10uF", "50V", "1206", "1276-6736-1-ND"],
+    ["470nF", "25V", "0603", "1276-2082-1-ND"],
+    ["1nF", "50V", "0603", "1276-1091-1-ND"],
+    ["2.2nF", "100V", "0603", "1276-6583-1-ND"],
+    ["100pF", "50V", "0603", "1276-1008-1-ND"]
+    ]
 
 import csv
 
-
-
 def GetCapacitorPN(input_value, package, voltageRating):
+    if("DNP" in input_value):
+        return ""
+    
     input_value = input_value.replace('f', 'F')
     input_value = input_value.replace('U', 'u')
     input_value = input_value.replace('N', 'n')
@@ -15,13 +25,20 @@ def GetCapacitorPN(input_value, package, voltageRating):
     #save the multiplier of the code
     if('p' in input_value):
         letter = 'p'
-        multiplier = 10**-12
     elif('n' in input_value):
         letter = 'n'
-        multiplier = 10**-9
     elif('u' in input_value):
         letter = 'u'
-        multiplier = 10**-6
+    else:
+        return ""
+        
+    if(float(input_value.split(letter)[0])<1):
+        letterOld = letter
+        if(letter == 'n'):
+            letter = 'p'
+        elif(letter == 'u'):
+            letter = 'n'
+        input_value = str(float(input_value.split(letterOld)[0]) * 1000)[:3] + letter + "F" #my lazy ass attempt to go from float to int is to splice off the decimal point. 
     VoltageInt = float(voltageRating.split("V")[0])
     for parts in Capacitors:
         if(input_value in parts[0]): #Match Capacitance
@@ -29,8 +46,6 @@ def GetCapacitorPN(input_value, package, voltageRating):
                 if(VoltageInt <= float(parts[1].split("V")[0])): #check voltage rating to be higher or equal
                     return parts[3]
     return "" 
-
-
 
 with open('George.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
